fe8f881b5b0f511b612cd862ecc752fb3ad4f519
Thiago Lugli <thiagoelg@gmail.com>
2019-09-16 17:00:53 -0300

    Fix for windows

--- src/macros.hh
+++ src/macros.hh
@@ -113,7 +113,7 @@
 
 #define REQUIRE_ARGUMENT_STRINGW(args, i, var)                                        \
     ARG_CHECK_STRING(args, i);                                                       \
-    v8::Local<v8::String> var(V8_LOCAL_STRING_FROM_VALUE(args[i])); \
+    v8::String::Value var(args[i]->ToString()); \
 
 
 #define OPTIONAL_ARGUMENT_FUNCTION(i, var)                                     \
