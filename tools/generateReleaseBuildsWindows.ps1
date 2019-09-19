69a319eacc12dcf17a2418014f4a2ea2eb97d047
Thiago Lugli <thiago.lugli@ifood.com.br>
2019-09-19 10:49:04 -0700

    Fix windows release script

--- tools/generateReleaseBuildsWindows.ps1
+++ tools/generateReleaseBuildsWindows.ps1
@@ -1,10 +1,12 @@
 param (
-  [Parameter(Mandatory=$true)][string]$release,
+  [Parameter(Mandatory=$true)][string]$release
 )
 
 $SOURCE_PATH = split-path -parent $MyInvocation.MyCommand.Definition
 $RELEASE_VERSION = $release
-$PACKAGE_VERSION = &"node -pe `"require('./package.json').version`""
+$PACKAGE_VERSION = node -pe "require('./package.json').version"
+
+echo $SOURCE_PATH
 
 $node_versions = @(
   "0.10.48",
@@ -31,28 +33,30 @@ $electron_versions = @(
 )
 
 # remove old build directory
-Remove-Item -Recurse -Force "$SOURCHE_PATH/build" | Out-Null
+Remove-Item -Recurse -Force $SOURCE_PATH'\..\build' -ErrorAction Ignore | Out-Null
 
 # create release path
-New-Item "$SOURCE_PATH/releases/$RELEASE_VERSION" -ItemType Directory -ea 0 | Out-Null
+New-Item $SOURCE_PATH'\..\releases\'$RELEASE_VERSION -ItemType Directory -ea 0 | Out-Null
 
 foreach ($version in $node_versions) {
   Write-Output "Building for node version: $version..."
-  node-pre-gyp configure --target=$version --module_name=node_printer | Out-Null
-  node-pre-gyp build package --target=$version --target_arch=x64 --build-from-source | Out-Null
-  node-pre-gyp configure --target=$version --module_name=node_printer | Out-Null
-  node-pre-gyp build package --target=$version --target_arch=ia32 --build-from-source | Out-Null
-  rsync -a -v "$SOURCE_PATH/build/stage/$PACKAGE_VERSION/" "$SOURCE_PATH/releases/$RELEASE_VERSION/" --remove-source-files | Out-Null
+  node-pre-gyp configure --target=$version --module_name=node_printer --silent | Out-Null
+  node-pre-gyp build package --target=$version --target_arch=x64 --build-from-source --silent | Out-Null
+  node-pre-gyp configure --target=$version --module_name=node_printer --silent | Out-Null
+  node-pre-gyp build package --target=$version --target_arch=ia32 --build-from-source --silent | Out-Null
+  Copy-item -Force -Recurse $SOURCE_PATH'\..\build\stage\'$PACKAGE_VERSION\* -Destination $SOURCE_PATH'\..\releases\'$RELEASE_VERSION -ErrorAction Ignore | Out-Null
+  Remove-Item -Recurse -Force $SOURCE_PATH'\..\build\stage' | Out-Null
   Write-Output "Done"
 }
 
 foreach ($version in $electron_versions) {
   Write-Output "Building for electron version: $version..."
-  node-pre-gyp configure --target=$version --dist-url=https://electronjs.org/headers --module_name=node_printer | Out-Null
-  node-pre-gyp build package --target=$version --target_arch=x64 --runtime=electron --build-from-source | Out-Null
-  node-pre-gyp configure --target=$version --dist-url=https://electronjs.org/headers --module_name=node_printer | Out-Null
-  node-pre-gyp build package --target=$version --target_arch=ia32 --runtime=electron --build-from-source | Out-Null
-  rsync -a -v "$SOURCE_PATH/build/stage/$PACKAGE_VERSION/" "$SOURCE_PATH/releases/$RELEASE_VERSION/" --remove-source-files | Out-Null
+  node-pre-gyp configure --target=$version --dist-url=https://electronjs.org/headers --module_name=node_printer --silent | Out-Null
+  node-pre-gyp build package --target=$version --target_arch=x64 --runtime=electron --build-from-source --silent | Out-Null
+  node-pre-gyp configure --target=$version --dist-url=https://electronjs.org/headers --module_name=node_printer --silent | Out-Null
+  node-pre-gyp build package --target=$version --target_arch=ia32 --runtime=electron --build-from-source --silent | Out-Null
+  Copy-item -Force -Recurse $SOURCE_PATH'\..\build\stage\'$PACKAGE_VERSION\* -Destination $SOURCE_PATH'\..\releases\'$RELEASE_VERSION -ErrorAction Ignore | Out-Null
+  Remove-Item -Recurse -Force $SOURCE_PATH'\..\build\stage' | Out-Null
   Write-Output "Done"
 }
 
